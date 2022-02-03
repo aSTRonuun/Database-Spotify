225765b8df999e17f47ab5e208f10dbd257d8ef7
Henryck <58481753+CarlosHenryck@users.noreply.github.com>
2022-02-03 01:33:44 -0300

    Create selectTables.sql

---
+++ database/selectTables.sql
@@ -0,0 +1,25 @@
+/* Musicas */
+select * from musica;
+select * from artista;
+select * from album;
+select * from turne;
+
+/* Podcast */ 
+select * from episodio;
+select * from podcast;
+select * from podcaster;
+
+/* Geral */
+select * from biblioteca;
+select * from ouvinte;
+select * from playlist;
+select * from audio_playlist;
+
+
+select musica.genero, count(musica), trunc(avg(musica.duracao), 2)
+from biblioteca 
+inner join playlist on biblioteca.id_biblioteca = playlist.id_biblioteca
+inner join audio_playlist on audio_playlist.id_playlist = playlist.id_playlist
+inner join musica on musica.id_musica = audio_playlist.id_musica
+group by musica.genero
+having avg(musica.duracao) > 3;
