fd4c6441953269b5e02c0bb8cdee608e415711e3
Henryck <58481753+CarlosHenryck@users.noreply.github.com>
2022-02-03 16:06:24 -0300

    Update createTablesUpdate.sql

--- database/createTablesUpdate.sql
+++ database/createTablesUpdate.sql
@@ -1,5 +1,17 @@
 create table turne(
 	id_turne serial primary key,
 	data date,
-	descricao varchar(100)
+	descricao varchar(100),
+	id_artista integer
 );
+
+create table artista(
+	id_artista serial primary key,
+	sobre varchar(100),
+	nome varchar(50),
+	qtd_ouvintes integer,
+	id_biblioteca integer
+);
+
+alter table turne add constraint fk_id_artista 
+foreign key(id_artista) references artista(id_artista);
