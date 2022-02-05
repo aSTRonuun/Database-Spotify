f6b4514cfd74c5bf8ca36c424e7ecb9e58180e98
Henryck <58481753+CarlosHenryck@users.noreply.github.com>
2022-02-05 02:02:51 -0300

    Update insertTables.sql

--- database/insertTables.sql
+++ database/insertTables.sql
@@ -31,7 +31,7 @@ values ('Carlos Henryck', 'false', 'henryck@gmail.com', 'pfjadspoiw', '17-07-200
 update biblioteca set id_user = 2 where id_biblioteca = 2;
 
 
-/* Inserir playlist*/
+/* Inserir playlists*/
 insert into playlist (titulo, descricao, autor, duracao_total, curtidas, qtd_musicas)
 values ('Classical Essentials', 'A selection of the greatest classical tunes', 'Mateus Santos',
 17, 10000, 148);
@@ -70,3 +70,164 @@ values ('Relaxing spanish guitar', 'The beautiful sound of the Spanish guitar to
 insert into playlist (titulo, descricao, autor, duracao_total, curtidas, qtd_musicas)
 values ('Sad Guitar', 'Instrumental guitar for rainy day contemplation',
 		'Alane Alves', 5, 33311, 124);
+		
+		
+/* Inserir playlists na biblioteca */
+insert into biblioteca_playlist(id_playlist, id_biblioteca)
+values (1, 1);
+
+insert into biblioteca_playlist(id_playlist, id_biblioteca)
+values (2, 1);
+
+insert into biblioteca_playlist(id_playlist, id_biblioteca)
+values (3, 1);
+
+insert into biblioteca_playlist(id_playlist, id_biblioteca)
+values (4, 1);
+
+insert into biblioteca_playlist(id_playlist, id_biblioteca)
+values (5, 1);
+
+insert into biblioteca_playlist(id_playlist, id_biblioteca)
+values (6, 2);
+
+insert into biblioteca_playlist(id_playlist, id_biblioteca)
+values (7, 2);
+
+insert into biblioteca_playlist(id_playlist, id_biblioteca)
+values (8, 2);
+
+insert into biblioteca_playlist(id_playlist, id_biblioteca)
+values (9, 2);
+
+insert into biblioteca_playlist(id_playlist, id_biblioteca)
+values (10, 2);
+
+/* Inserir Artistas */ 
+insert into artista (nome, sobre, qtd_ouvintes) 
+values ('Jacob Collier', 'The most innovative musicians of his generation', 1704189);
+
+insert into artista (nome, sobre, qtd_ouvintes) 
+values ('Lizzy McAlpine', 'glad you could make it', 3083244);
+
+insert into artista (nome, sobre, qtd_ouvintes) 
+values ('Billie Eilish', 'Eilish continues to shatter the ceiling of music with her genre-defying sound', 56540683);
+
+insert into artista (nome, sobre, qtd_ouvintes) 
+values ('Rafael Alves', 'Rafael Alves se tornou referência no estilo Fingerstyle no Brasil', 11855);
+
+insert into artista (nome, sobre, qtd_ouvintes) 
+values ('Olivia Rodrigo', 'I’m 18 years old and writing songs is my favorite thing to do in the world.', 13447723);
+
+insert into artista (nome, sobre, qtd_ouvintes) 
+values ('Lewis Capaldi', 'the number 1 spot at American radio.', 9192745);
+
+insert into artista (nome, sobre, qtd_ouvintes) 
+values ('Harry Styles', 'Fine Line out now.', 18838395);
+
+insert into artista (nome, sobre, qtd_ouvintes) 
+values ('Lil Nas X', 'Lil Nas X is a 22-year-old award-winning rapper, singer, songwriter', 51254709);
+
+insert into artista (nome, sobre, qtd_ouvintes) 
+values ('The Kid LAROI', 'the newly eighteen year old rapper/singer born Charlton Howard', 48935954);
+
+insert into artista (nome, sobre, qtd_ouvintes) 
+values ('FINNEAS', 'Bios are boring', 8625614);
+
+/* Inserir artistas na biblioteca */
+insert into biblioteca_artista(id_artista, id_biblioteca)
+values (1, 1);
+
+insert into biblioteca_artista(id_artista, id_biblioteca)
+values (2, 1);
+
+insert into biblioteca_artista(id_artista, id_biblioteca)
+values (3, 1);
+
+insert into biblioteca_artista(id_artista, id_biblioteca)
+values (4, 1);
+
+insert into biblioteca_artista(id_artista, id_biblioteca)
+values (5, 1);
+
+insert into biblioteca_artista(id_artista, id_biblioteca)
+values (6, 2);
+
+insert into biblioteca_artista(id_artista, id_biblioteca)
+values (7, 2);
+
+insert into biblioteca_artista(id_artista, id_biblioteca)
+values (8, 2);
+
+insert into biblioteca_artista(id_artista, id_biblioteca)
+values (9, 2);
+
+insert into biblioteca_artista(id_artista, id_biblioteca)
+values (10, 2);
+
+/* Inserir Albums */ 
+insert into album (qtd_musica, titulo, duracao_total, descricao, id_artista)
+values (2, 'Djesse Vol.2', 1, '', 1);
+
+insert into album (qtd_musica, titulo, duracao_total, descricao, id_artista)
+values (2, 'Give Me A Minute', 1.5, '', 2);
+
+insert into album (qtd_musica, titulo, duracao_total, descricao, id_artista)
+values (2, 'Dont smile at me', 0.3, '', 3);
+
+insert into album (qtd_musica, titulo, duracao_total, descricao, id_artista)
+values (2, 'Fingerstyle Gospel', 0.5, '', 4);
+
+insert into album (qtd_musica, titulo, duracao_total, descricao, id_artista)
+values (2, 'SOUR', 1.2, '', 5);
+
+insert into album (qtd_musica, titulo, duracao_total, descricao, id_artista)
+values (2, 'Divinely Uninspired', 0.9, '', 6);
+
+insert into album (qtd_musica, titulo, duracao_total, descricao, id_artista)
+values (2, 'Fine Line', 0.65, '', 7);
+
+insert into album (qtd_musica, titulo, duracao_total, descricao, id_artista)
+values (2, 'MONTERO', 1, '', 8);
+
+insert into album (qtd_musica, titulo, duracao_total, descricao, id_artista)
+values (2, 'F*CK LOVE', 1.2, '', 9);
+
+insert into album (qtd_musica, titulo, duracao_total, descricao, id_artista)
+values (2, 'Optimist', 0.8, '', 10);
+
+/* Inserir albums na biblioteca */
+
+insert into biblioteca_album (id_album, id_biblioteca) 
+values(1, 1);
+
+insert into biblioteca_album (id_album, id_biblioteca) 
+values(2, 1);
+
+insert into biblioteca_album (id_album, id_biblioteca) 
+values(3, 1);
+
+insert into biblioteca_album (id_album, id_biblioteca) 
+values(4, 1);
+
+insert into biblioteca_album (id_album, id_biblioteca) 
+values(5, 1);
+
+insert into biblioteca_album (id_album, id_biblioteca) 
+values(6, 2);
+
+insert into biblioteca_album (id_album, id_biblioteca) 
+values(7, 2);
+
+insert into biblioteca_album (id_album, id_biblioteca) 
+values(8, 2);
+
+insert into biblioteca_album (id_album, id_biblioteca) 
+values(9, 2);
+
+insert into biblioteca_album (id_album, id_biblioteca) 
+values(10, 2);
+
+/* Inserir Músicas */
+
+
