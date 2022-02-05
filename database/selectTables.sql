8d8e70103653b7ee1cc87df479cb79393e7b4765
Henryck <58481753+CarlosHenryck@users.noreply.github.com>
2022-02-05 15:21:44 -0300

    Update selectTables.sql

--- database/selectTables.sql
+++ database/selectTables.sql
@@ -15,28 +15,17 @@ select * from ouvinte;
 select * from playlist;
 select * from audio_playlist;
 
-/*Selects para auxiliar */
-
-	/*Select de todas as playlists a partir de um ouvinte */
-	select playlist.titulo, playlist.autor from ouvinte 
-	join biblioteca on ouvinte.id_biblioteca = biblioteca.id_biblioteca
-	join biblioteca_playlist on biblioteca.id_biblioteca = biblioteca_playlist.id_biblioteca
-	join playlist on playlist.id_playlist = biblioteca_playlist.id_playlist
-	where ouvinte.id_user = 1;
-	
-	/*Select de album por artista */
-	select artista.nome, album.titulo from album
-	join artista on album.id_artista = artista.id_artista;
-
 /* Buscar por músicas em uma playlist da biblioteca, separando elas por gênero */
-select musica.genero, count(musica), trunc(avg(musica.duracao), 2)
-from biblioteca 
-inner join playlist on biblioteca.id_biblioteca = playlist.id_biblioteca
-inner join audio_playlist on audio_playlist.id_playlist = playlist.id_playlist
-inner join musica on musica.id_musica = audio_playlist.id_musica
-where playlist.id_playlist = 1
-group by musica.genero
-having avg(musica.duracao) > 3;
+select m.genero, count(m), trunc(avg(m.duracao), 2)
+from biblioteca as b
+inner join ouvinte as o on b.id_user = o.id_user
+inner join biblioteca_playlist as bp on b.id_biblioteca = bp.id_biblioteca
+inner join playlist as p on p.id_playlist = bp.id_playlist
+inner join audio_playlist as ap on ap.id_playlist = p.id_playlist
+inner join musica as m on m.id_musica = ap.id_musica
+where b.id_biblioteca = 1 and p.id_playlist = 1
+group by m.genero
+having avg(m.duracao) > 2;
 
 /*  Buscar por nomes artistas com musicas de um determinado genero */
 SELECT A.nome
@@ -48,14 +37,14 @@ WHERE exists (
 )
 
 /*  Buscar por informações de artistas com turnes depois de certa data, e com uma restrição de ouvintes */
-select * from artista as a
+sselect * from artista as a
 full outer join turne as t on a.id_artista = t.id_artista
 where exists(
 	select data from turne 
 	where data > '25-03-2022'
-) and a.qtd_ouvintes >= any(
+) and a.qtd_ouvintes = any(
 	select qtd_ouvintes from artista
-	where qtd_ouvintes > 0 and qtd_ouvintes < 200
+	where qtd_ouvintes > 10000 and qtd_ouvintes < 8000000
 );
 
 /* Dado um ouvinte, buscar por todas as playlists que estao em sua biblioteca */
