9922b38ed5482a6054a4ec1c5a3989232215a33a
Henryck <58481753+CarlosHenryck@users.noreply.github.com>
2022-02-06 12:01:10 -0300

    Update Triggers.sql

--- database/Triggers.sql
+++ database/Triggers.sql
@@ -43,3 +43,62 @@ AFTER DELETE
 ON biblioteca_playlist
 FOR EACH ROW
 EXECUTE PROCEDURE att_qtd_playlist_delete_in_lib()
+
+/*Stored procedure para verificar inserção na playlist */
+CREATE or replace FUNCTION inserirPlaylist (id_musicaF integer, id_playlistF integer)
+RETURNS boolean
+LANGUAGE PLPGSQL
+AS
+$$
+DECLARE
+	exists integer = null;
+	qtd_musicaPlaylist integer;
+BEGIN
+	select qtd_musicas into qtd_musicaPlaylist from playlist
+	where id_playlist = id_playlistF;
+	
+	IF qtd_musicaPlaylist >= 150 THEN
+		 raise notice 'Playlist cheia';
+		 return false;
+	END IF;		 
+	
+	select id_musica into exists from audio_playlist 
+ 	where id_musica = id_musicaF
+ 	and id_playlist = id_playlistF;
+	
+	IF exists is null THEN
+		raise notice 'Música foi adicionada';
+		return true;
+	END IF;
+	
+	raise notice 'Música já está na playlist';
+	return false;
+END;
+$$;
+
+
+/*Trigger para verificar playlist usando o storade procedure */
+CREATE or replace FUNCTION verificaPlaylist ()
+RETURNS TRIGGER	
+LANGUAGE PLPGSQL
+AS
+$$
+DECLARE
+-- seu código aqui
+BEGIN
+  	IF inserirPlaylist(new.id_musica, new.id_playlist) THEN
+		update playlist set qtd_musicas = qtd_musicas + 1 where id_playlist = new.id_playlist;
+		return new;
+	ELSE 
+		return null;
+	END IF;		
+END;
+$$;
+
+CREATE TRIGGER verificaPlaylistTrigger
+BEFORE INSERT ON audio_playlist
+FOR EACH ROW
+EXECUTE PROCEDURE verificaPlaylist();
+
+
+
